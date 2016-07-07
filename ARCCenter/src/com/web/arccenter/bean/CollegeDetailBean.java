76181b9ea9052b4344477ae0296e82a2fac8ae1c
Prash123v <prash_123v@rediffmail.com>
2016-07-07 22:03:59 +0530

    Created new java bean CollegeDetailBean.java

---
+++ ARCCenter/src/com/web/arccenter/bean/CollegeDetailBean.java
@@ -0,0 +1,77 @@
+package com.web.arccenter.bean;
+
+public class CollegeDetailBean
+{
+	private String  collegename;
+	private int establishedYear;
+	private String university;
+	private String phone ;
+	private String fax;
+	private String email;
+	private String address;
+	private String city;
+	private String state;
+	private String country;
+	
+	public String getCollegename() {
+		return collegename;
+	}
+	public void setCollegename(String collegename) {
+		this.collegename = collegename;
+	}
+	
+	public int getEstablishedYear() {
+		return establishedYear;
+	}
+	public void setEstablishedYear(int establishedYear) {
+		this.establishedYear = establishedYear;
+	}
+	public String getUniversity() {
+		return university;
+	}
+	public void setUniversity(String university) {
+		this.university = university;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getFax() {
+		return fax;
+	}
+	public void setFax(String fax) {
+		this.fax = fax;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}	
+}
