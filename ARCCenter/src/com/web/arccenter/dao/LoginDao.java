dfa8fdabfa7c0f35e114e89896a7778838022a44
Prash123v <prash_123v@rediffmail.com>
2016-07-07 21:56:38 +0530

    Adding New Project Empty Structure

---
+++ ARCCenter/src/com/web/arccenter/dao/LoginDao.java
@@ -0,0 +1,11 @@
+package com.web.arccenter.dao;
+
+import java.sql.ResultSet;
+
+import com.web.arccenter.bean.LoginBean;
+import com.web.arccenter.databaseconnection.MySqlHelper;
+
+public class LoginDao 
+{
+	
+}
---
+++ ARCCenter/src/com/web/arccenter/databaseconnection/MySqlHelper.java
@@ -0,0 +1,13 @@
+package com.web.arccenter.databaseconnection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.Properties;
+
+public class MySqlHelper 
+{
+	
+}
+
